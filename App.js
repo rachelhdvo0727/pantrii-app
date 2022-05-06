e27b6e9e5383b937bd29b663340ea597f66e5b06
Tiffany Tran <tiffany@raahandel.dk>
2022-05-06 19:12:08 +0200

    fonts and iconns added

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
+const loadFonts = () => {
+    return Font.loadAsync({
+        'TT-Commons-Bold': require('./assets/fonts/TT-Commons-Bold.otf'),
+        'TT-Commons-DemiBold': require('./assets/fonts/TT-Commons-DemiBold.otf'),
+        'TT-Commons-Medium': require('./assets/fonts/TT-Commons-Medium.otf'),
+        'TT-Commons-Regular': require('./assets/fonts/TT-Commons-Regular.otf'),
+    });
+};
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hi</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [fontLoaded, setFontLoaded] = React.useState(false);
+    if (!fontLoaded) {
+        return (
+            <AppLoading
+                startAsync={loadFonts}
+                onFinish={() => setFontLoaded(true)}
+                onError={(error) => console.error(error)}
+            />
+        );
+    }
+    return (
+        <View style={styles.container}>
+            <Text style={{ fontFamily: 'TT-Commons-Bold' }}>Hi</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ styles/General.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export default generalStyles = StyleSheet.create({
+    headerH1: {
+        fontSize: 26,
+        color: '#FFFFFF',
+        fontFamily: 'TT-Common-Bold',
+    },
+});
