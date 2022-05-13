91efed49348e9f185a7117f500a17e52bcab8ee2
rachelvo <rachelhd.vo@outlook.com>
2022-05-13 21:31:50 +0200

    cleaned up

--- routes/Navigation.js
+++ routes/Navigation.js
@@ -6,7 +6,6 @@ import {
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BottomTabBuyers from './screens/buyers/BottomTabBuyers';
 // import BottomTabSuppliers from './screens/suppliers/BottomTabSuppliers';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 
 const Stack = createNativeStackNavigator();
