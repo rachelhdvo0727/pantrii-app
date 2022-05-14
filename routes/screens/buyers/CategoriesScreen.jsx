b39cbfa5ca723906dc7c2da55a569b9444006c20
Tiffany Tran <tiffany.tran1212@gmail.com>
2022-05-14 16:43:37 +0200

    mongodb codes

--- routes/screens/buyers/CategoriesScreen.jsx
+++ routes/screens/buyers/CategoriesScreen.jsx
@@ -1,20 +1,50 @@
 import React from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
-import CategoryCard from '../../../components/buyers/CategoryCard';
-import CategoryData from '../../../dummy-data/CategoriesData.js';
 import generalStyles from '../../../styles/General';
+import axios from 'axios';
 
 export default function CategoriesScreen() {
+    const data = JSON.stringify({
+        collection: 'categories',
+        database: 'pantriiapp',
+        dataSource: 'PantriiApp',
+    });
+    const config = {
+        method: 'post',
+        url: 'https://data.mongodb-api.com/app/data-oxvtw/endpoint/data/beta/action/find',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Request-Headers': '*',
+            'Api-key':
+                '2GZQT0hpTykckgCnk5ajds55663JisDpmQg3r9gy94YhgO9rDay9NeEzClKm6jcc',
+        },
+        data: data,
+    };
+    const [categories, setCategories] = React.useState(null);
+    React.useEffect(() => {
+        axios(config)
+            .then(function (response) {
+                setCategories(response.data?.documents);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }, []);
+
+    React.useEffect(() => {
+        console?.log(categories);
+    }, []);
     return (
         <View style={generalStyles.container}>
             <FlatList
-                columnWrapperStyle={generalStyles.spaceBetween}
-                numColumns={2}
-                data={CategoryData}
-                keyExtractor={(item) => item.id}
+                data={categories?.length > 0 ? categories : null}
+                keyExtractor={(item) => item?._id?.$oid}
                 renderItem={({ item }) => (
-                    <CategoryCard title={item.title} imageSrc={item.imgUrl} />
+                    <View key={item?._id?.$oid}>
+                    <Text>{item.name}</Text>
+                    </View>
                 )}
+                scrollEnabled={true}
             ></FlatList>
         </View>
     );
