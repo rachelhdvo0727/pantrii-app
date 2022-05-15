1fff6687bd083ca973ef8634e6fb319e06188e7b
rachelvo <rachelhd.vo@outlook.com>
2022-05-15 18:23:37 +0200

    mapped image relative paths

---
+++ dummy-data/images.js
@@ -0,0 +1,11 @@
+export const categoryImages = {
+    'bread.png': require('../assets/categories/bread.png'),
+    'colonial.png': require('../assets/categories/colonial.png'),
+    'dairy.png': require('../assets/categories/dairy.png'),
+    'dessert.png': require('../assets/categories/dessert.png'),
+    'drinks.png': require('../assets/categories/drinks.png'),
+    'fish-and-seafood.png': require('../assets/categories/fish-and-seafood.png'),
+    'fruits-and-veggies.png': require('../assets/categories/fruits-and-veggies.png'),
+    'meat.png': require('../assets/categories/meat.png'),
+    'plantbased.png': require('../assets/categories/plantbased.png'),
+};
