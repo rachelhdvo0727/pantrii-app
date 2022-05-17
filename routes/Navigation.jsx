409ca977406b2cf6232e2633448f40ffbe478e92
rachelvo <rachelhd.vo@outlook.com>
2022-05-17 23:27:14 +0200

    mock log in

--- routes/Navigation.jsx
+++ routes/Navigation.jsx
@@ -4,15 +4,41 @@ import {
     getFocusedRouteNameFromRoute,
 } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import BottomTabBuyers from './screens/buyers/BottomTabBuyers';
-// import BottomTabSuppliers from './screens/suppliers/BottomTabSuppliers';
+// Components
 import { StyleSheet } from 'react-native';
+import BottomTabBuyers from './screens/buyers/BottomTabBuyers';
+import LogInScreen from './screens/LogInScreen';
+import SignUpScreen from './screens/SignUpScreen';
+
+// import BottomTabSuppliers from './screens/suppliers/BottomTabSuppliers';
 
 const Stack = createNativeStackNavigator();
 export default function Navigation() {
+    // TODO: use actual login
+    const loggedInUser = false;
     return (
         <NavigationContainer>
-            <BottomTabBuyers />
+            {loggedInUser ? (
+                <BottomTabBuyers />
+            ) : (
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="LogInScreen"
+                        component={LogInScreen}
+                        options={{
+                            headerShown: false,
+                        }}
+                    />
+                    <Stack.Screen
+                        name="SignUpScreen"
+                        component={SignUpScreen}
+                        options={{
+                            headerShown: false,
+                        }}
+                    />
+                </Stack.Navigator>
+            )}
+
             {/* <BottomTabSuppliers /> */}
         </NavigationContainer>
     );
