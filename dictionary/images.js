aff7477372bc09c1583f4956f285bafb8f60e872
Tiffany Tran <tiffany.tran1212@gmail.com>
2022-05-17 14:52:27 +0200

    backend on product sliders started

--- dictionary/images.js
+++ dictionary/images.js
@@ -10,3 +10,16 @@ export const categoryImages = {
     'meat.png': require('../assets/categories/meat.png'),
     'plantbased.png': require('../assets/categories/plantbased.png'),
 };
+
+export const productImages = {
+    'all-products.png': require('../assets/categories/all-products.png'),
+    'bread.png': require('../assets/categories/bread.png'),
+    'colonial.png': require('../assets/categories/colonial.png'),
+    'dairy.png': require('../assets/categories/dairy.png'),
+    'dessert.png': require('../assets/categories/dessert.png'),
+    'drinks.png': require('../assets/categories/drinks.png'),
+    'fish-and-seafood.png': require('../assets/categories/fish-and-seafood.png'),
+    'fruits-and-veggies.png': require('../assets/categories/fruits-and-veggies.png'),
+    'meat.png': require('../assets/categories/meat.png'),
+    'plantbased.png': require('../assets/categories/plantbased.png'),
+};
