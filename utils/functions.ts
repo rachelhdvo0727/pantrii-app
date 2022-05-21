c7c8e7253ba497ea07e0fa4b49df49118765843c
rachelvo <rachelhd.vo@outlook.com>
2022-05-21 21:08:06 +0200

    correct argument

--- utils/functions.ts
+++ utils/functions.ts
@@ -1,6 +1,6 @@
 import * as SecureStore from 'expo-secure-store';
 
-export const objectToString = (object: Object) => JSON.stringify(obj);
+export const objectToString = (object: Object) => JSON.stringify(object);
 
 export async function saveData(key: string, value: string) {
     await SecureStore.setItemAsync(key, value);
