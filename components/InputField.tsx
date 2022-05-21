70d4682c588bccf431948f0c83ccda8d6bfe65e2
rachelvo <rachelhd.vo@outlook.com>
2022-05-22 00:28:08 +0200

    fix show/hide bug on password input

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -54,14 +54,14 @@ export default function InputField({
                     multiline={multiline}
                     autoCapitalize={autoCapitalize}
                     autoComplete={autoComplete}
-                    secureTextEntry={isPasswordInput}
+                    secureTextEntry={showPassword}
                     maxLength={maxLength}
                     blurOnSubmit
                     style={styles.textValue}
                 ></TextInput>
                 {isPasswordInput ? (
                     <AntDesign
-                        name={showPassword ? 'eyeo' : 'eye'}
+                        name={showPassword ? 'eye' : 'eyeo'}
                         size={17}
                         color="black"
                         style={styles.eyeIcon}
