979b9de8a621d87705afb6f30eccf137785106f8
rachelvo <rachelhd.vo@outlook.com>
2022-05-31 18:53:55 +0200

    cleaned up

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import Navigation from './routes/Navigation';
@@ -26,12 +24,3 @@ export default function App() {
     }
     return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
