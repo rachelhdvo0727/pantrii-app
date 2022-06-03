737dcc28220c583c7eb26519e4da5a660ac754c2
rachelvo <rachelhd.vo@outlook.com>
2022-06-03 22:05:32 +0200

    set up all products view

--- routes/screens/suppliers/ProductsScreen.jsx
+++ routes/screens/suppliers/ProductsScreen.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import generalStyles from '../../../styles/General';
+import { sortOptions } from '../../../utils/variables';
+// Components
+import { StyleSheet, Text, View } from 'react-native';
+import SelectDropDown from '../../../components/SelectDropDown';
 
 export default function ProductsSuppliersScreen() {
+    const [selectedSort, setSelectedSort] = React.useState(sortOptions[0]);
+    const onSelectedSort = (item) => {
+        setSelectedSort(item);
+    };
     return (
         <View style={generalStyles.container}>
-            <Text style={generalStyles.headerH1}>Produkter</Text>
+            <SelectDropDown
+                label="Sortér efter"
+                data={sortOptions}
+                onSelect={onSelectedSort}
+                selectedItem={selectedSort}
+            />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        ...generalStyles.container,
+    },
+});
