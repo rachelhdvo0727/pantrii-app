c35daf629723a3f6f2bcc6156880730b744160ba
rachelvo <rachelhd.vo@outlook.com>
2022-06-03 17:22:14 +0200

    tested components for supplier's home

--- routes/screens/suppliers/HomeScreen.jsx
+++ routes/screens/suppliers/HomeScreen.jsx
@@ -4,42 +4,64 @@ import { useNavigation } from '@react-navigation/native';
 // Component
 import { StyleSheet, Text, View } from 'react-native';
 import HeroCard from '../../../components/buyers/HeroCard';
+import ProductCard from '../../../components/suppliers/ProductCard';
+import SystemMessageBanner from '../../../components/SystemMessageBanner';
 import CampaignCardSlider from '../../../components/buyers/CampaignCardSlider';
 import Button from '../../../components/actions/Button';
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome as Entypo } from '@expo/vector-icons';
 
 export default function HomeSuppliersScreen(props) {
     const navigation = useNavigation();
     React.useEffect(() => {
-        console.log(props);
+        console.log('home-supplier', props);
     }, []);
 
     const handleCreateProduct = () => {
         navigation.navigate('Opret');
     };
+
     return (
         <View style={styles.container}>
             <View style={styles.section}>
                 <HeroCard
-                    title={`Velkommen tilbage`}
+                    title={`Velkommen tilbage \n ${props.route?.params?.user?.firstName}`}
                     secondary
                     imageSrc={require('../../../assets/banners/producer-banner-home.png')}
                 />
                 <Button
                     primary
                     title="&emsp;Opret vare"
-                    children={
-                        <FontAwesome name="plus" size={15} color="#FFFFFF" />
-                    }
+                    children={<Entypo name="plus" size={15} color="#FFFFFF" />}
                     onPress={handleCreateProduct}
                     buttonStyle={styles.buttonStyle}
                 />
             </View>
             <View style={styles.section}>
                 <Text style={styles.headers}>opdateringer</Text>
+                <View style={styles.sectionContent}>
+                    <SystemMessageBanner
+                        text="Vi ændrer vores leveringsgebyr til 50,00 kr./ order"
+                        isWarning
+                    />
+                    <SystemMessageBanner
+                        text="Din vare nr. 123456 er godkendt og oprettet"
+                        isConfirmation
+                    />
+                </View>
             </View>
             <View style={styles.section}>
                 <Text style={styles.headers}>dine varer</Text>
+                <View style={styles.sectionContent}>
+                    <ProductCard
+                        productTitle="HI"
+                        amount="3"
+                        productDesc="lorem ipsum"
+                        productUnit="3 x 100g"
+                        bulkPrice="10000"
+                        singlePrice="20000"
+                        isLowOnStock
+                    />
+                </View>
             </View>
             <CampaignCardSlider />
         </View>
@@ -50,6 +72,11 @@ const styles = StyleSheet.create({
     container: { ...generalStyles.homeContainer },
     section: {
         marginHorizontal: 15,
+        marginVertical: 10,
+    },
+    sectionContent: {
+        justifyContent: 'space-evenly',
+        marginVertical: 10,
     },
     buttonStyle: {
         alignSelf: 'flex-end',
