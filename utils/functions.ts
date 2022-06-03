ff909126ec85baf0eac85da90b2afbb9b6e48b54
rachelvo <rachelhd.vo@outlook.com>
2022-06-04 00:48:34 +0200

    Update functions.ts

--- utils/functions.ts
+++ utils/functions.ts
@@ -5,3 +5,8 @@ export const objectToString = (object: Object) => JSON.stringify(object);
 export async function saveData(key: string, value: string) {
     await SecureStore.setItemAsync(key, value);
 }
+
+// Capitalize 1st letter in a sentence/string
+export const capitalize = (string: string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
