ff77d63976de87a68941ea78e5458fd2ad9041fe
rachelvo <rachelhd.vo@outlook.com>
2022-06-05 10:56:51 +0200

    Fixed merge - store

--- redux/store.js
+++ redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { categoriesSlice } from './slice/categories';
 import { userSlice } from './slice/user';
 import { rolesSlice } from './slice/roles';
+import cartReducer from './reducer/CartReducer';
 
 export const store = configureStore({
     reducer: {
         user: userSlice.reducer,
         roles: rolesSlice.reducer,
         categories: categoriesSlice.reducer,
+        cart: cartReducer,
     },
 });
