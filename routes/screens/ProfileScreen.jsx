9257f0c17c4a0decfe60d73c9ffc3fb8f7bcc038
rachelvo <rachelhd.vo@outlook.com>
2022-06-05 11:40:12 +0200

    cleaned up and added saved user from store

--- routes/screens/ProfileScreen.jsx
+++ routes/screens/ProfileScreen.jsx
@@ -10,50 +10,44 @@ import HeroCard from '../../components/buyers/HeroCard';
 import InformationCard from '../../components/InformationCard';
 import SectionInInformationCard from '../../components/SectionInInformationCard';
 // API
-import axios from 'axios';
-import { findUser } from '../../utils/api';
-import { useSelector, useDispatch } from 'react-redux';
-import { getUser } from '../../redux/slice/user';
+import { useSelector } from 'react-redux';
 
 export default function ProfileScreen(props) {
     const content = dictionary?.customerTypes;
     const navigation = useNavigation();
     const { user } = useSelector((state) => state.user);
+    const loggedInUser = props?.route?.params.loggedInUser;
     const userRole = props?.route?.params?.currentRole;
 
     const onEdit = (information) => {
         navigation.navigate('ProfileEditScreen', {
-            user: user || props?.route?.params.loggedInUser,
+            user: user || loggedInUser,
             informationType: information,
         });
     };
 
-    // const handleLogOut = () => {
-    //     SecureStore.setItemAsync('user', '');
-    // };
-
-    const handleLogOut = async function () {
-        return await SecureStore.setItemAsync('user', '')
-            .then(async () => {
-                // To verify that current user is now empty, currentAsync can be used
-                const currentUser = await SecureStore.getItemAsync('user');
-                if (currentUser === null) {
-                    console.log('Success!', 'No user is logged in anymore!');
-                }
-                // Navigation dispatch calls a navigation action, and popToTop will take
-                // the user back to the very first screen of the stack
-                // navigation.dispatch(StackActions.popToTop());
-                return true;
-            })
-            .catch((error) => {
-                console.log('Error!', error.message);
-                return false;
-            });
+    const handleLogOut = () => {
+        SecureStore.setItemAsync('user', '');
     };
 
-    React.useEffect(() => {
-        // props.navigation.popToTop();
-    });
+    // const handleLogOut = async function () {
+    //     return await SecureStore.setItemAsync('user', '')
+    //         .then(async () => {
+    //             // To verify that current user is now empty, currentAsync can be used
+    //             const currentUser = await SecureStore.getItemAsync('user');
+    //             if (currentUser === null) {
+    //                 console.log('Success!', 'No user is logged in anymore!');
+    //             }
+    //             // Navigation dispatch calls a navigation action, and popToTop will take
+    //             // the user back to the very first screen of the stack
+    //             // navigation.dispatch(StackActions.popToTop());
+    //             return true;
+    //         })
+    //         .catch((error) => {
+    //             console.log('Error!', error.message);
+    //             return false;
+    //         });
+    // };
 
     const ProfileInformation = () => (
         <SectionInInformationCard
@@ -62,16 +56,19 @@ export default function ProfileScreen(props) {
             sectionContent={
                 <React.Fragment>
                     <Text style={styles.highlightText}>
-                        {(user || props?.route?.params.loggedInUser)?.firstName}{' '}
-                        {(user || props?.route?.params.loggedInUser)?.lastName}
+                        {(user || loggedInUser)?.firstName}{' '}
+                        {(user || loggedInUser)?.lastName}
                     </Text>
-                    <Text style={styles.text}>
-                        {(user || props?.route?.params.loggedInUser).email}
-                    </Text>
-
-                    <Text style={styles.text}>
-                        {(user || props?.route?.params.loggedInUser)?.phone}
-                    </Text>
+                    {(user || loggedInUser)?.email && (
+                        <Text style={styles.text}>
+                            {(user || loggedInUser)?.email}
+                        </Text>
+                    )}
+                    {(user || loggedInUser)?.phone && (
+                        <Text style={styles.text}>
+                            {(user || loggedInUser)?.phone}
+                        </Text>
+                    )}
                 </React.Fragment>
             }
             isEditable
@@ -87,34 +84,19 @@ export default function ProfileScreen(props) {
             sectionContent={
                 <React.Fragment>
                     <Text style={styles.text}>
-                        {
-                            (user || props?.route?.params.loggedInUser)?.address
-                                ?.line1
-                        }
-                        {
-                            (user || props?.route?.params.loggedInUser)?.address
-                                ?.line2
-                        }
+                        {(user || loggedInUser)?.address?.line1}
+                        {(user || loggedInUser)?.address?.line2}
                     </Text>
                     <View style={styles.cityWrapper}>
                         <Text style={styles.text}>
-                            {
-                                (user || props?.route?.params.loggedInUser)
-                                    ?.address?.zipCode
-                            }
+                            {(user || loggedInUser)?.address?.zipCode}
                         </Text>
                         <Text style={styles.text}>
-                            {
-                                (user || props?.route?.params.loggedInUser)
-                                    ?.address?.city
-                            }
+                            {(user || loggedInUser)?.address?.city}
                         </Text>
                     </View>
                     <Text style={styles.text}>
-                        {
-                            (user || props?.route?.params.loggedInUser)?.address
-                                ?.country
-                        }
+                        {(user || loggedInUser)?.address?.country}
                     </Text>
                 </React.Fragment>
             }
