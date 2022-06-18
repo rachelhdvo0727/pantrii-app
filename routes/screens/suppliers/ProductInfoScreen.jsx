3bcee3268e0f3c3d148cdeea0e988907154f1608
rachelvo <rachelhd.vo@outlook.com>
2022-06-19 01:16:18 +0200

    Update ProductInfoScreen.jsx

--- routes/screens/suppliers/ProductInfoScreen.jsx
+++ routes/screens/suppliers/ProductInfoScreen.jsx
@@ -5,19 +5,11 @@ import categoryDictionary from '../../../dictionary/categories.json';
 import { numberFormat, capitalize } from '../../../utils/functions';
 import { productImages } from '../../../dictionary/images';
 import { useNavigation } from '@react-navigation/native';
-import { useForm, Controller } from 'react-hook-form';
 // Components
 import {
     StyleSheet,
-    View,
-    Text,
-    ScrollView,
-    SafeAreaView,
-    Image,
-    Dimensions,
+
 } from 'react-native';
-import InputField from '../../../components/InputField';
-import InputFieldSelect from '../../../components/InputFieldSelect';
 import BackIconButton from '../../../components/actions/BackIconButton';
 import ProductInfoCard from '../../../components/buyers/ProductInfoCard';
 // API & Redux
