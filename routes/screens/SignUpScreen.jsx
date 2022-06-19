9fb43c402589a4966ba961f44109d10fb0a19d9c
rachelvo <rachelhd.vo@outlook.com>
2022-06-19 21:49:10 +0200

    cleaned up packages

--- routes/screens/SignUpScreen.jsx
+++ routes/screens/SignUpScreen.jsx
@@ -14,8 +14,6 @@ import ApprovedModal from '../../components/ApprovedModal';
 // API
 import { createUser } from '../../redux/slice/createUser';
 import { useDispatch, useSelector } from 'react-redux';
-import * as Crypto from 'expo-crypto';
-const CryptoJS = require('crypto-js');
 
 export default function LogInScreen(props) {
     const navigation = useNavigation();
